fc46a2d00027d7c995d0b8dc4328faaba5922b9f
Jeff Linahan <jeff.linahan@gmail.com>
2018-02-17 03:09:17 -0500

    split up each graph into its own unit test

--- unittest.cpp
+++ unittest.cpp
@@ -47,7 +47,102 @@ void check_partition_is_legal(string graphfile)
 	//partition.print();
 }
 
-BOOST_AUTO_TEST_CASE( my_test )
+BOOST_AUTO_TEST_CASE( gmy_test )
+{
+	check_partition_is_legal("graphs/g");
+}
+
+BOOST_AUTO_TEST_CASE( box2my_test )
+{
+	check_partition_is_legal("graphs/box2");
+}
+
+BOOST_AUTO_TEST_CASE( box3my_test )
+{
+	check_partition_is_legal("graphs/box3");
+}
+
+BOOST_AUTO_TEST_CASE( emptymy_test )
+{
+	check_partition_is_legal("graphs/empty");
+}
+
+//BOOST_AUTO_TEST_CASE( hugemy_test )
+//{
+//	check_partition_is_legal("graphs/huge");
+//}
+
+BOOST_AUTO_TEST_CASE( inmy_test )
 {
 	check_partition_is_legal("graphs/in");
+}
+
+BOOST_AUTO_TEST_CASE( in2my_test )
+{
+	check_partition_is_legal("graphs/in2");
+}
+
+BOOST_AUTO_TEST_CASE( in3my_test )
+{
+	check_partition_is_legal("graphs/in3");
+}
+
+BOOST_AUTO_TEST_CASE( in4my_test )
+{
+	check_partition_is_legal("graphs/in4");
+}
+
+BOOST_AUTO_TEST_CASE( insideoutmy_test )
+{
+	check_partition_is_legal("graphs/insideout");
+}
+
+BOOST_AUTO_TEST_CASE( kuratowski33my_test )
+{
+	check_partition_is_legal("graphs/kuratowski33");
+}
+
+BOOST_AUTO_TEST_CASE( kuratowski5my_test )
+{
+	check_partition_is_legal("graphs/kuratowski5");
+}
+
+BOOST_AUTO_TEST_CASE( notkmy_test )
+{
+	check_partition_is_legal("graphs/notk");
+}
+
+BOOST_AUTO_TEST_CASE( randmy_test )
+{
+	check_partition_is_legal("graphs/rand");
+}
+
+BOOST_AUTO_TEST_CASE( rand2my_test )
+{
+	check_partition_is_legal("graphs/rand2");
+}
+
+BOOST_AUTO_TEST_CASE( rand3my_test )
+{
+	check_partition_is_legal("graphs/rand3");
+}
+
+BOOST_AUTO_TEST_CASE( squaremy_test )
+{
+	check_partition_is_legal("graphs/square");
+}
+
+BOOST_AUTO_TEST_CASE( tiemy_test )
+{
+	check_partition_is_legal("graphs/tie");
+}
+
+BOOST_AUTO_TEST_CASE( trimy_test )
+{
+	check_partition_is_legal("graphs/tri");
+}
+
+BOOST_AUTO_TEST_CASE( twomy_test )
+{
+	check_partition_is_legal("graphs/two");
 }
